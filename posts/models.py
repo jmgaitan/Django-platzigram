e777b45ac7bbe146dfc97286f6db07453788fed2
Juan Manuel Gaitan <jmgaitan93@gmail.com>
2020-11-12 22:14:18 -0300

    crear una base y generar tablas y consultas

--- posts/models.py
+++ posts/models.py
@@ -3,15 +3,27 @@
 
 from django.db import models
 
+###Creamos una tabla en la BBDD ###
 
-class user(models.Model):
+class User(models.Model):
 
         email = models.EmailField(unique=True)
         password = models.CharField(max_length=100)
         first_name= models.CharField(max_length=100)
         last_name = models.CharField(max_length=100)
+
+        is_admin = models.BooleanField(default=False)
         bio = models.TextField(blank=True)
         birthdate = models.DateField(blank=True, null=True)
         created = models.DateTimeField(auto_now_add=True)
         modified = models.DateTimeField(auto_now=True)
+
+        def __str__(self):
+                return self.email
+
+### Luego de crear la tabla vamos a correr el comando > py manage.py makemigrations ###
+### luego de esto corremos el > py manage.py migrate ###
+### Ya la tabla estará generada ###
+
+""" para grabar datos debemos ir a > py manage.py shell """
 
